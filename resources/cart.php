616921fd985ea49b0ccda8ed5ecc02a5acb650f1
tikarammardi <tikarammardi007@gmail.com>
2018-11-11 06:37:36 +0530

    report functionalities almost done

--- resources/cart.php
+++ resources/cart.php
@@ -121,5 +121,68 @@ DELIMETER;
         }
      }
 
+
+
+     function report() {
+
+        global $connection;
+
+        if(isset($_GET['tx'])){
+
+            $amount = $_GET['amt'];
+            $currency = $_GET['cc'];
+            $transaction = $_GET['tx'];
+            $status = $_GET['st'];
+
+            
+
+
+        $total = 0;
+        $item_quantity = 0;
+      
+    
+        foreach($_SESSION as $name => $value) {
+    
+            if($value > 0) {
+    
+                if(substr($name, 0, 8 ) == "product_") {
+    
+                    $length = strlen($name);
+                    $id = substr($name, 8, $length);
+                    $query = query("SELECT * FROM products WHERE product_id = ". escape_string($id)." ");
+                    confirm($query);
+                
+                    while($row = fetch_array($query)) {
+
+                        $sub = $row['product_price'] * $value;
+                        $item_quantity += $value;
+                        $product_price = $row['product_price'];
+                        $product_title = $row['product_title'];
+                        
+                        
+                  
+            $send_order = query("INSERT INTO orders (order_amount, order_transaction, order_status, order_currency) VALUES('{$amount}','{$transaction}','{$status}','{$currency}')");
+
+            $last_id = last_id();
+            echo $last_id;
+            
+            confirm($send_order);
+                        
+            $insert_report = query("INSERT INTO reports (product_id,order_id,product_title, product_price, product_quantity) VALUES('{$id}','{$last_id}','{$product_title}','{$product_price}','{$value}')");
+            confirm($insert_report);
+                
+                }
+                     $total += $sub;
+                    echo $item_quantity;
+                }
+        
+            }
+    
+        }
+    }else {
+        redirect("index.php");
+    }  
+}
+
 ?>
 
--- resources/functions.php
+++ resources/functions.php
@@ -2,6 +2,12 @@
 
 //helper functions
 
+function last_id() {
+    global $connection;
+
+    return mysqli_insert_id($connection);
+}
+
 function set_message($msg) {
     if(!empty($msg)) {
         $_SESSION['message'] = $msg;
