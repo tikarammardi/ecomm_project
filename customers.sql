60e8ba80b25866db98c392425b8270e879cc174d
Tikaram Mardi <tikarammardi007@gmail.com>
2018-12-10 00:04:16 +0530

    trigger implemented

--- customers.sql
+++ customers.sql
@@ -7,3 +7,19 @@ ALTER TABLE reports
 ADD product_id INT(11)
 REFERENCES products(product_id) 
 ON DELETE SET NULL;
+
+
+$sql = "CREATE TRIGGER `updateProductPrice`\n"
+
+    . "BEFORE UPDATE ON `products`\n"
+
+    . "FOR EACH ROW\n"
+
+    . "BEGIN\n"
+
+    . "  IF NEW.product_price <> OLD.product_price\n"
+
+    . "    THEN\n"
+
+    . "      SET NEW.product_price = NEW.product_price * 5.0
+    END IFEND";
